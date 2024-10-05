2d3a08f0d25bac3c969dbdc92b7a0b4a535cdf37
roblocscripter <gamemastersalt3wastaken@gmail.com>
2024-10-05 11:44:17 -0400

    Update lancetloadstring.lua

--- lancetloadstring.lua
+++ lancetloadstring.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/rosploiter/scripts/refs/heads/main/lancet.lua",true))()
 --Lancet in loadstring format
-print(lancet load success)
+print("lancet load success")
